a9f6df25c8640c38f6bcba28c2c1789939ac2acc
sonalishelkar <98012257+sonalishelkar@users.noreply.github.com>
2022-04-01 15:03:21 +0530

    quotient&remainder

---
+++ assign1.java
@@ -0,0 +1,17 @@
+
+// Java Program to Compute Quotient and Remainder
+
+public class assign1
+ {
+
+  public static void main(String[] args) {
+
+    int dividend = 40, divisor = 6;
+
+    int quotient = dividend / divisor;
+    int remainder = dividend % divisor;
+
+    System.out.println("Quotient = " + quotient);
+    System.out.println("Remainder = " + remainder);
+  }
+}
